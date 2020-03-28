14805117615fd250feb882cdb344930b2977074b
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-28 20:48:40 -0300

    Adds Text, images and missing imports for used components

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import heroesLogo from '../../assets/logo.png';
+
+import style from './style'
 
 export default function Incidents() {
   return(
     <View style={style.container}>
       <View style={style.header}>
-        <Image source={}/>
+        <Image source={heroesLogo}/>
         <Text style={style.headerText}>
           Total: <Text style={style.headerTextBold}>0 Incidents</Text>
         </Text>
       </View>
       <View>
-        <Text style={style.title}></Text>
-        <Text style={style.description}></Text>
+        <Text style={style.title}>Welcome!</Text>
+        <Text style={style.description}>
+          Choose one of the incidents below and save the day!
+        </Text>
       </View>
     </View>
   );
