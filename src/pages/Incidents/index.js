561c905685200f6b614aaf8dbdb819e01eeff7fd
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-28 20:09:53 -0300

    Updates incidents page with it's intended initial content

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default function Home() {
+export default function Incidents() {
   return(
-    <View />
+    <View style={style.container}>
+      <View style={style.header}>
+        <Image source={}/>
+        <Text style={style.headerText}>
+          Total: <Text style={style.headerTextBold}>0 Incidents</Text>
+        </Text>
+      </View>
+      <View>
+        <Text style={style.title}></Text>
+        <Text style={style.description}></Text>
+      </View>
+    </View>
   );
 }
