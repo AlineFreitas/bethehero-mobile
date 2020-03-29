1401c15f0ff9f416ea414424106f98d242441602
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 01:42:35 -0300

    Makes whatsapp and e-mail contact buttons functional

--- src/pages/Incident/index.js
+++ src/pages/Incident/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity} from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
 
 import heroesLogo from '../../assets/logo.png';
 
@@ -10,8 +11,22 @@ import style from './style';
 export default function Incident() {
   const navigation = useNavigation();
 
+  const message = `Hello! I'm getting in touch to help ou on your case "Amora", with an amount of R$100,00`;
+
   function navigateBack(){
-    navigation.goBack();
+    navigation.goBack(); 
+  }
+
+  function sendEmail(){
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Amora',
+      recipients: [''],
+      body: message,
+    });
+  }
+
+  function sendWhatsapp(){
+    Linking.openURL(`whatsapp://send?phone=5522998777382&text=${message}`)
   }
 
   return(
@@ -38,12 +53,12 @@ export default function Incident() {
         <Text style={style.contactText}>Get in Touch</Text>
 
         <View style={style.contacts}>
-          <TouchableOpacity style={style.contact} onPress={() => {}}>
-            <Text style={style.actionText}> E-mail</Text>
+          <TouchableOpacity style={style.contact} onPress={sendEmail}>
+            <Text style={style.actionText}>E-mail</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={style.contact} onPress={() => {}}>
-            <Text style={style.actionText}> Whatsapp</Text>
+          <TouchableOpacity style={style.contact} onPress={sendWhatsapp}>
+            <Text style={style.actionText}>Whatsapp</Text>
           </TouchableOpacity>
         </View>
       </View>
