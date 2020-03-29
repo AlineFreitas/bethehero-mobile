98a0a6f1d316c25cde7f20598f8457199b1c4945
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 15:56:45 -0300

    Adds 'infinite scroll' to incidents page

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -10,7 +10,11 @@ import style from './style'
 
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]); 
-  const [totalIncidents, setTotalIncidents] = useState(0); 
+  const [totalIncidents, setTotalIncidents] = useState(0);
+
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
   function navigateToShowIncident(incident) {
@@ -18,9 +22,24 @@ export default function Incidents() {
   }
 
   async function loadIncidents(){
-    const response = await api.get('/incidents');
-    setIncidents(response.data);
+    if (loading){
+      return ;
+    }
+
+    if (totalIncidents > 0 & incidents.length == totalIncidents) {
+      return ;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('/incidents', {
+      params: page
+    });
+    setIncidents([...incidents, ...response.data]);
     setTotalIncidents(response.headers['x-total-count']);
+
+    setPage(page+1);
+    setLoading(false);
   }
 
   useEffect(() => { loadIncidents() },
@@ -48,6 +67,8 @@ export default function Incidents() {
         data={incidents}
         keyExtractor={ incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         style={style.incidentsList}
         renderItem={ ( { item: incident }) => (
           <View style={style.incident}>
