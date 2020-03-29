38d55388436ca1f5c286dfd41d14d618eb25fd34
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 14:29:01 -0300

    Updates Incident page to lreceive the data from Incidents page and display it

--- src/pages/Incident/index.js
+++ src/pages/Incident/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 
 import heroesLogo from '../../assets/logo.png';
@@ -10,6 +10,9 @@ import style from './style';
 
 export default function Incident() {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const incident = route.params.incident;
 
   const message = `Hello! I'm getting in touch to help ou on your case "Amora", with an amount of R$100,00`;
 
@@ -39,12 +42,19 @@ export default function Incident() {
       </View>
 
       <View style={style.incident}>
-        <Text style={[style.incidentLabel, { marginTop: 0 }]}>Incident</Text>
-        <Text style={style.incidentValue}>Amora</Text>
-        <Text style={style.incidentLabel}>Description</Text>
-        <Text style={style.incidentValue}>Something something, with Amora.</Text>
+        <Text style={[style.incidentLabel, { marginTop: 0 }]}>NGO</Text>
+        <Text style={style.incidentValue}>{incident.name} from {incident.city}/{incident.UF}</Text>
+        <Text style={style.incidentLabel}>Title</Text>
+        <Text style={style.incidentValue}> {incident.title} </Text>
+        <Text style={style.incidentLabel}>More details</Text>
+        <Text style={style.incidentValue}>{incident.description}</Text>
         <Text style={style.incidentLabel}>Value</Text>
-         <Text style={style.incidentValue}>R$500,00</Text>
+        <Text style={style.incidentValue}>
+          { Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(incident.value)}
+        </Text>
       </View>
 
       <View style={style.contactBox}>
