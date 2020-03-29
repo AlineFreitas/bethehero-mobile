25422c87cd1e3e8d75ca4d5dd916d3f36a352e84
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-28 22:29:18 -0300

    Added List of incidents to Incidents page, temporary with static values

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
 import { View, Image, Text } from 'react-native';
 
 import heroesLogo from '../../assets/logo.png';
 
 import style from './style'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Incidents() {
   return(
@@ -19,6 +21,68 @@ export default function Incidents() {
         <Text style={style.description}>
           Choose one of the incidents below and save the day!
         </Text>
+      </View>
+      <View style={style.incidentsList}>
+        <View style={style.incident}>
+          <Text style={style.incidentLabel}>Incident</Text>
+          <Text style={style.incidentValue}>Amora</Text>
+          <Text style={style.incidentLabel}>Description</Text>
+          <Text style={style.incidentValue}>Something something, with Amora.</Text>
+          <Text style={style.incidentLabel}>Value</Text>
+          <Text style={style.incidentValue}>R$500,00</Text>
+
+          <TouchableOpacity
+            style={style.showIncidentButton}
+            onPress={ () => {}} >
+              <Text style={style.showIncidentButtonText}>See Details</Text>
+              <Feather name="arrow-right" size={17} color="#e02041"/>
+          </TouchableOpacity>
+        </View>
+        <View style={style.incident}>
+          <Text style={style.incidentLabel}>Incident</Text>
+          <Text style={style.incidentValue}>Amora</Text>
+          <Text style={style.incidentLabel}>Description</Text>
+          <Text style={style.incidentValue}>Something something, with Amora.</Text>
+          <Text style={style.incidentLabel}>Value</Text>
+          <Text style={style.incidentValue}>R$500,00</Text>
+
+          <TouchableOpacity
+            style={style.showIncidentButton}
+            onPress={ () => {}} >
+              <Text style={style.showIncidentButtonText}>See Details</Text>
+              <Feather name="arrow-right" size={17} color="#e02041"/>
+          </TouchableOpacity>
+        </View>
+        <View style={style.incident}>
+          <Text style={style.incidentLabel}>Incident</Text>
+          <Text style={style.incidentValue}>Amora</Text>
+          <Text style={style.incidentLabel}>Description</Text>
+          <Text style={style.incidentValue}>Something something, with Amora.</Text>
+          <Text style={style.incidentLabel}>Value</Text>
+          <Text style={style.incidentValue}>R$500,00</Text>
+
+          <TouchableOpacity
+            style={style.showIncidentButton}
+            onPress={ () => {}} >
+              <Text style={style.showIncidentButtonText}>See Details</Text>
+              <Feather name="arrow-right" size={17} color="#e02041"/>
+          </TouchableOpacity>
+        </View>
+        <View style={style.incident}>
+          <Text style={style.incidentLabel}>Incident</Text>
+          <Text style={style.incidentValue}>Amora</Text>
+          <Text style={style.incidentLabel}>Description</Text>
+          <Text style={style.incidentValue}>Something something, with Amora.</Text>
+          <Text style={style.incidentLabel}>Value</Text>
+          <Text style={style.incidentValue}>R$500,00</Text>
+
+          <TouchableOpacity
+            style={style.showIncidentButton}
+            onPress={ () => {}} >
+              <Text style={style.showIncidentButtonText}>See Details</Text>
+              <Feather name="arrow-right" size={17} color="#e02041"/>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
